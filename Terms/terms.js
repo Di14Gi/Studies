f07b95b3f5e2c279384c3389f5146863dd1626cb
Dima <gsuev9500@mail.ru>
2022-12-07 02:13:55 +0400

    writing conditions

---
+++ Terms/terms.js
@@ -0,0 +1,36 @@
+// if(4 == 9){
+//     console.log('Ok');
+// }else {
+//     console.log('Error');
+// }
+
+
+// const num = 50;
+
+// if(num < 49){
+//     console.log('Error');
+// }else if(num > 100){
+//     console.log('Много');
+// }else {
+//     console.log('Oke');
+// }
+
+
+// (num == 50) ? console.log('Oke') : console.log('Error');
+
+
+const num = 50;
+switch (num){
+    case 48:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('Oke');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
